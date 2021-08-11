c702083cac2655c8ebb1e343afc7caa4aab3416f
thoraya-atef <thorayaatef22@gmail.com>
2021-08-11 19:42:02 +0200

    fix ui

--- lib/tasbeeh.dart
+++ lib/tasbeeh.dart
@@ -24,94 +24,96 @@ class _tasbeehState extends State<tasbeeh> {
         ),
 
         child: Center(
-            child: ListView(
-              children: [
-                Row(
-                  children: [ Expanded(
-                    child:Text('إسلامي',
-                    style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: 'ELMessiri',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30),
-                    textAlign: TextAlign.center,
-                   ),
-                ),
-                  ]),
-
-                seb7a(degreeOfRotate(rotate)),
-                Container(
-                  margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                  child:Text('عدد التسبيحات',style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'ELMessiri',
-                    fontSize: 20,),
-                    textAlign: TextAlign.center,
+            child: Expanded(
+              child: ListView(
+                children: [
+                  Row(
+                    children: [ Expanded(
+                      child:Text('إسلامي',
+                      style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'ELMessiri',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),
+                      textAlign: TextAlign.center,
+                     ),
                   ),
+                    ]),
 
+                  seb7a(degreeOfRotate(rotate)),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                    child:Text('عدد التسبيحات',style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'ELMessiri',
+                      fontSize: 20,),
+                      textAlign: TextAlign.center,
+                    ),
+
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                      Expanded(
+                      child:Container(
+
+                          decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                           color: Color.fromRGBO(
+                               183, 147, 95, 0.5),
+
+                  ),
+                  margin: EdgeInsets.fromLTRB(150, 30, 150, 10),
+                  child: Text('$counter',style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,),
+                  textAlign: TextAlign.center,
+                  ),
+                        padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                        height:60 ,
+                     width: 90,
+                  ),
+                  ),
+                      ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                Row( crossAxisAlignment:CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+
                 children: [
                     Expanded(
                     child:Container(
 
-                        decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                         color: Color.fromRGBO(
-                             183, 147, 95, 0.5),
+                      child: ElevatedButton(
+                      onPressed: () { incrementCounter(); },
+                      child: Text(typeTasbeeh,
+                      style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'ReemKufi',
+                      fontSize: 25)),
+                      style: ButtonStyle(
+                      backgroundColor:MaterialStateProperty.all<Color>(
+                      Color.fromRGBO(183, 147, 95, 0.9882352941176471),
+                      ),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                              )
+                          ),
+                      ),
 
-                ),
-                margin: EdgeInsets.fromLTRB(150, 30, 150, 10),
-                child: Text('$counter',style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,),
-                textAlign: TextAlign.center,
-                ),
-                      padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                      height:60 ,
-                   width: 90,
-                ),
-                ),
-                    ],
-              ),
-              Row( crossAxisAlignment:CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-
-              children: [
-                  Expanded(
-                  child:Container(
-
-                    child: ElevatedButton(
-                    onPressed: () { incrementCounter(); },
-                    child: Text(typeTasbeeh,
-                    style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'ReemKufi',
-                    fontSize: 25)),
-                    style: ButtonStyle(
-                    backgroundColor:MaterialStateProperty.all<Color>(
-                    Color.fromRGBO(183, 147, 95, 0.9882352941176471),
+                    ) ,
+                      height: 40,
+                      margin: EdgeInsets.fromLTRB(100, 40, 100, 30),
                     ),
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20.0),
-                            )
-                        ),
-                    ),
-
-                  ) ,
-                    height: 40,
-                    margin: EdgeInsets.fromLTRB(100, 40, 100, 30),
                   ),
-                ),
-              ],
-            ),
+                ],
+              ),
        ],
     ),
+            ),
 
           ),
         ),
