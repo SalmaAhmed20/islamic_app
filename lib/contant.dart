dff1d847493cc4c2cb3518cbac24b2c11aaaf076
Esraa-99 <66671227+Esraa-99@users.noreply.github.com>
2021-08-11 10:34:22 +0200

    loading any content of the user's choice

--- lib/contant.dart
+++ lib/contant.dart
@@ -4,47 +4,51 @@ import 'package:flutter/services.dart' show rootBundle;
 import 'package:quran_app/main.dart';
 import 'main.dart';
 
-class contant extends State<MyApp> {
+  class contant extends State<MyApp> {
+    String name='';
+    contant(String name) {
+      this.name = name;
+    }
   String data='';
   fetchFileData() async{
-    String responseText;
-    responseText = await rootBundle.loadString('asset/files/1.txt');
-    setState(() {
-      data= responseText;
-    });
+  String responseText;
+  responseText = await rootBundle.loadString('asset/files/$name.txt');
+  setState(() {
+  data= responseText;
+  });
   }
   @override
   void initState() {
-    fetchFileData();
-    super.initState();
+  fetchFileData();
+  super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:new Stack(
-        children: <Widget>[
-          new Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image:new AssetImage("asset/bg3@3x.png"),
-                      fit: BoxFit.fill
-                  ),
+  return Scaffold(
+  body:new Stack(
+  children: <Widget>[
+  new Container(
+  decoration: BoxDecoration(
+  image: DecorationImage(
+  image:new AssetImage("asset/bg3@3x.png"),
+  fit: BoxFit.fill
+  ),
 
-              ),
-          ),
-              new Center(
-                  child:new Text(parse(data),style: TextStyle(fontSize:28),textDirection:TextDirection.rtl),
+  ),
+  ),
+  new Center(
+  child:new Text(parse(data),style: TextStyle(fontSize:28),textDirection:TextDirection.rtl),
 
-          )
-        ]
-      )
-      );
+  )
+  ]
+  )
+  );
 
   }
-}
-String parse(String data){
+  }
+  String parse(String data){
   return data.replaceAllMapped('\n', (match) => '(*)');
-}
+  }
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,6 @@ void main() {
 class MyApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
-  contant createState() => contant();
+  contant createState() => contant("1");
 
 }
