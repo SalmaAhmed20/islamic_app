45fe5f2375a07e9bbfe689c3d383a6b029e3901c
Roka Adel <rokaadel808@gamil.com>
2021-08-13 18:02:34 +0200

    ahadeth

---
+++ lib/hadeth3.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class App3 extends StatelessWidget {
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home :Scaffold(backgroundColor: Colors.white30,
+        )
+    );
+  }
+}
