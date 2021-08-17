aeb564566ac5f94d069b91431b0d031a8cb4127a
SalmaAhmed20 <64385957+SalmaAhmed20@users.noreply.github.com>
2021-08-17 21:14:32 +0200

    solve Qur'an  ui error

--- lib/SuraContent.dart
+++ lib/SuraContent.dart
@@ -48,7 +48,7 @@ class _contantState extends State<contant> {
               image: DecorationImage(
                   image: new AssetImage(provider.isDark()
                       ? "assets/images/Dark/bg1@2x.png"
-                      : "assets/images/2.0x/bg2@2x.png"),
+                      : "assets/images/2.0x/bg3@2x.png"),
                   fit: BoxFit.fill),
             ),
             child: Center(
